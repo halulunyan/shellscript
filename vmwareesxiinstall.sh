52dd34d943ab0015f7fd393b6f04bff282af4846
halu <halulunyan@users.noreply.github.com>
2018-06-03 03:06:25 +0900

    Add files via upload

---
+++ vmwareesxiinstall.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+
+# Ensure we only use unmodified commands
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin
+
+# Check for old unlcoker and issue message
+if [ -f /bootbank/darwin.vgz ]; then
+   echo Error - please remove the older unlocker first!
+   exit 1
+fi
+# Copy patch local.sh
+if [ -d /scratch ]; then
+    echo Installing local.sh 
+    cp local.sh /etc/rc.local.d/local.sh
+    echo Success - please now restart the server!
+    exit 0
+else
+    echo Error - no scratch partition!
+    exit 1
+fi
